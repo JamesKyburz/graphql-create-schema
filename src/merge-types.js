975d609fb79e0595887fa78f267ee06220a15b19
James Kyburz <james.kyburz@gmail.com>
2018-05-10 18:02:57 +0200

    query and mutation optional

--- src/merge-types.js
+++ src/merge-types.js
@@ -1,13 +1,11 @@
 module.exports = merge
 
 function merge (types) {
+  const query = `${types.map(x => x.typeDefs && x.typeDefs.query)}`
+  const mutation = `${types.map(x => x.typeDefs && x.typeDefs.mutation)}`
   return `
     ${types.map(x => x.typeDefs && x.typeDefs.types)}
-    type Query {
-      ${types.map(x => x.typeDefs && x.typeDefs.query)}
-    }
-    type Mutation {
-      ${types.map(x => x.typeDefs && x.typeDefs.mutation)}
-    }
+    ${query ? `type Query {${query}}` : ''}
+    ${mutation ? `type Mutation {${mutation}}` : ''}
   `
 }
