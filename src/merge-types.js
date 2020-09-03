d4d94a675edf1910844ed7330802d15552cf8b36
James Kyburz <james.kyburz@gmail.com>
2020-09-03 11:26:54 +0100

    fix: types optional too

--- src/merge-types.js
+++ src/merge-types.js
@@ -8,7 +8,7 @@ function merge (types) {
     .map(x => x.typeDefs && x.typeDefs.mutation)
     .filter(Boolean)}`
   return `
-    ${types.map(x => x.typeDefs && x.typeDefs.types)}
+    ${types.map(x => x.typeDefs && x.typeDefs.types).filter(Boolean)}
     ${query ? `type Query {${query}}` : ''}
     ${mutation ? `type Mutation {${mutation}}` : ''}
   `
