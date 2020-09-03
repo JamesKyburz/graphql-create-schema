33e24ec8f7725d1e213da6f55e145c4845bedf85
James Kyburz <james.kyburz@gmail.com>
2020-09-03 11:19:47 +0100

    fix: pass graphql-tools function as a reference

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const { makeExecutableSchema } = require('graphql-tools')
 const mergeResolvers = require('./merge-resolvers')
 const mergeTypes = require('./merge-types')
 
 module.exports = createSchema
 
-function createSchema (types) {
+function createSchema (makeExecutableSchema, types) {
   types = Array.isArray(types) ? types : [types]
   return makeExecutableSchema({
     typeDefs: mergeTypes(types),
